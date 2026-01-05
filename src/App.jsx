10d4c9966ab42efc2fc1d00d5612b65b7b933adf
Rajat Bhawsar <rajatbhawsar430@gmail.com>
2026-01-05 21:45:11 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React Version 2 Upgrade</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
